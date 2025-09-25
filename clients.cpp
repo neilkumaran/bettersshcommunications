70dbd8903be860c245ccb16219ed088151c154c1
neilkumaran <neil.kumaran@mcvts.org>
2025-09-25 12:23:45 -0400

    combined test/testclient.cpp into clients.cpp to send data to server

--- clients.cpp
+++ clients.cpp
@@ -5,61 +5,95 @@
 #include <arpa/inet.h>
 #include <netinet/in.h>
 #include <cstring>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <string>
 
 using namespace std;
 
+// vpn filtering for tuns
 bool vpnCheck(const char* ifname) {
-    // vpn filtering for tuns
-    return (std::strncmp(ifname, "tun", 3) == 0 || std::strncmp(ifname, "wg", 2) == 0);
+    return (strncmp(ifname, "tun", 3) == 0 || strncmp(ifname, "wg", 2) == 0);
 }
 
+// virtual interfaces that need to be not added
 bool isVirtual(const char* ifname) {
-    // virtual interfaces that need to be not added
-    return (std::strncmp(ifname, "virbr", 5) == 0 ||  // libvirt
-            std::strncmp(ifname, "docker", 6) == 0 || // docker bridge
-            std::strncmp(ifname, "vboxnet", 7) == 0); // VirtualBox
+    return (strncmp(ifname, "virbr", 5) == 0 ||  // libvirt
+            strncmp(ifname, "docker", 6) == 0 || // docker bridge
+            strncmp(ifname, "vboxnet", 7) == 0); // VirtualBox
 }
 
 bool isPhysical(const char* ifname) {
     // the good stuff
-    return (std::strncmp(ifname, "eth", 3) == 0 ||
-            std::strncmp(ifname, "enp", 3) == 0 ||
-            std::strncmp(ifname, "wlan", 4) == 0 ||
-            std::strncmp(ifname, "wl", 2) == 0);
+    return (strncmp(ifname, "eth", 3) == 0 ||
+            strncmp(ifname, "enp", 3) == 0 ||
+            strncmp(ifname, "wlan", 4) == 0 ||
+            strncmp(ifname, "wl", 2) == 0);
 }
 
-std::string getLocalAddrInfo() {
+string getLocalAddrInfo() {
     ifaddrs *ifaddr;
     if (getifaddrs(&ifaddr) == -1) {
         perror("getifaddrs");
         return "";
     }
 
-    std::string result;
+    string result;
 
     for (ifaddrs *ifa = ifaddr; ifa; ifa = ifa->ifa_next) {
         if (!ifa->ifa_addr) continue;
+        if (ifa->ifa_addr->sa_family != AF_INET) continue;
 
-        if (ifa->ifa_addr->sa_family == AF_INET) {
-            sockaddr_in *sa = (sockaddr_in*)ifa->ifa_addr;
-            char addr[INET_ADDRSTRLEN];
-            inet_ntop(AF_INET, &sa->sin_addr, addr, sizeof(addr));
+        sockaddr_in *sa = (sockaddr_in*)ifa->ifa_addr;
+        char addr[INET_ADDRSTRLEN];
+        inet_ntop(AF_INET, &sa->sin_addr, addr, sizeof(addr));
 
-            if (std::strcmp(addr, "127.0.0.1") == 0) continue; // skip loopback
-            if (vpnCheck(ifa->ifa_name)) continue;                // skip VPNs
-            if (!isPhysical(ifa->ifa_name)) continue;         // only physical NICs
-            if (isVirtual(ifa->ifa_name)) continue;           // skip virtual bridges
+        if (!strcmp(addr, "127.0.0.1")) continue; // skip loopback
+        if (vpnCheck(ifa->ifa_name)) continue;    // skip VPNs
+        if (!isPhysical(ifa->ifa_name)) continue; // only physical NICs
+        if (isVirtual(ifa->ifa_name)) continue;   // skip virtual bridges
 
-            result = addr;  // take the first matching LAN IP
-            break;
-        }
+        result = addr;  // take the first matching LAN IP
+        break;
     }
 
     freeifaddrs(ifaddr);
     return result; // empty string if none found
 }
 
+void sendHeartbeat(string clientIP, string hostName, string serverIP) {
+    int sock = socket(AF_INET, SOCK_STREAM, 0);
+    
+    if (sock < 0) {
+        perror("socket init error");
+    }
+
+    sockaddr_in server_addr{};
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(12345);
+    server_addr.sin_addr.s_addr = inet_addr(serverIP.c_str());
+
+    if (connect(sock, (sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
+        perror("connect");
+    }
+
+    string message = hostName + ", " + clientIP; 
+
+    send(sock, message.c_str(), strlen(message.c_str()), 0);
+
+    close(sock);
+    
+}
+
+
 
 int main() {
-    cout << getLocalAddrInfo(); 
+    string serverIP;
+    string clientIP = getLocalAddrInfo();
+    cout << serverIP << endl;
+    const char* username = getenv("USER");
+
+    cout << "Enter server ip: "; 
+    cin >> serverIP; 
+    sendHeartbeat(clientIP, username, serverIP);
 }
